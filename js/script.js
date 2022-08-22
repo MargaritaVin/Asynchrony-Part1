507d76e7fe08e1cf0e24921707aa972d0add1b0e
MargaritaVin <ritasergienko@rambler.ru>
2022-08-22 16:22:31 +0300

    added new Promise in script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+let promise = new Promise (function(resolve, reject) {
+    setTimeout(() => resolve('done'), 5000);
+    setTimeout(() => reject(new Error('Error')), 5000);
+});
+
+promise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+  .finally(() => {
+    console.log('Promise was finished!')
+  });
+
+async function getPromise() {
+    try {
+      const result = await promise;
+      console.log(result);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      console.log('Promise was finished!');
+    }
+  };
+getPromise();
